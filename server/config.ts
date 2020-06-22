de7bfb5419eb10a5bd9b891a61a7da8bc4c34332
Cynicollision <stn825@gmail.com>
2020-06-22 16:09:36 -0400

    updating config placeholders

--- server/config.ts
+++ server/config.ts
@@ -1,8 +1,8 @@
 export class Config {
     static readonly authClientID = '2EHHIox2_2t01td8HfxYNpSuEZAVwLpH';
-    static readonly authJwksUri = 'https://brewkeeper.auth0.com/.well-known/jwks.json';
-    static readonly authUri = 'https://brewkeeper.auth0.com/';
-    static readonly mongo = process.env.MONGODB_URI || 'mongodb://localdev:l0cald3v@localhost:27017/brewkeeper'
+    static readonly authJwksUri = 'https://vastforce.auth0.com/.well-known/jwks.json';
+    static readonly authUri = 'https://vastforce.auth0.com/';
+    static readonly mongo = process.env.MONGODB_URI || 'mongodb://localdev:l0cald3v@localhost:27017/vastforce'
     static readonly dev = (process.env.NODE_ENV || 'development') === 'development';
     static readonly port = process.env.PORT || '3000';
 }
