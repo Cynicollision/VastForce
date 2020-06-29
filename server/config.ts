c83be4e726ad11ff4d1ada740a17e07f227ce5a3
Cynicollision <stn825@gmail.com>
2020-06-29 14:56:37 -0400

    updating server dev config

--- server/config.ts
+++ server/config.ts
@@ -1,7 +1,7 @@
 export class Config {
-    static readonly authClientID = '2EHHIox2_2t01td8HfxYNpSuEZAVwLpH';
-    static readonly authJwksUri = 'https://vastforce.auth0.com/.well-known/jwks.json';
-    static readonly authUri = 'https://vastforce.auth0.com/';
+    static readonly authClientID = 'FGnxJkAuROymM8pbg9IBz37KKG0xSMgs';
+    static readonly authJwksUri = 'https://stn-dev.us.auth0.com/.well-known/jwks.json';
+    static readonly authUri = 'https://stn-dev.us.auth0.com/';
     static readonly mongo = process.env.MONGODB_URI || 'mongodb://localdev:l0cald3v@localhost:27017/vastforce'
     static readonly dev = (process.env.NODE_ENV || 'development') === 'development';
     static readonly port = process.env.PORT || '3000';
