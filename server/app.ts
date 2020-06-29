d04b3f966e0c6781111c4709cfb06d67d1ff4766
Cynicollision <stn825@gmail.com>
2020-06-29 17:47:46 -0400

    app server refactor for flexibility

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,25 @@
 import * as express from 'express';
-import { VastForceAppServer } from './app-server';
+import { ExpressAppServer } from './app-server';
 import { AccountData } from './data/account-data';
 import { AccountLogic } from './logic/account-logic';
 import { ReportData } from './data/report-data';
 import { ReportLogic } from './logic/report-logic';
 import { OrgData } from './data/org-data';
 import { OrgDataLogic } from './logic/orgdata-logic';
+import { configureAPIRoutes } from './routes/api-resources';
+import { configureAuthRoutes } from './routes/auth';
 
 const accountData = new AccountData();
 const reportData = new ReportData();
 const orgData = new OrgData();
 
-new VastForceAppServer(
-    new AccountLogic(accountData),
-    new OrgDataLogic(accountData, orgData),
-    new ReportLogic(accountData, reportData),
-).start(express());
+const accountLogic = new AccountLogic(accountData);
+const orgDataLogic = new OrgDataLogic(accountData, orgData);
+const reportLogic = new ReportLogic(accountData, reportData);
+
+new ExpressAppServer(express())
+    .configure((app: express.Application) => {
+        configureAuthRoutes(app, accountLogic);
+        configureAPIRoutes(app, orgDataLogic, reportLogic);
+    })
+    .start();
