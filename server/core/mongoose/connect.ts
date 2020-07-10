e6dc45567e5069fc0a6620ad669cc01f7ac84d66
Cynicollision <stn825@gmail.com>
2020-07-10 16:40:34 -0400

    address deprecation warnings

--- server/core/mongoose/connect.ts
+++ server/core/mongoose/connect.ts
@@ -4,7 +4,10 @@ import { ResponseUtil } from './../util/response';
 
 export function connectMongoDB(mongoUri: string): Promise<OperationResponse<boolean>> {
     return new Promise<OperationResponse<boolean>>((resolve, reject) => {
+        mongoose.set('useUnifiedTopology', true);
+        mongoose.set('useCreateIndex', true);
         mongoose.connect(mongoUri, { useNewUrlParser: true });
+
         mongoose.connection.on('error', () => { 
             resolve(ResponseUtil.fail(`Database connection error.`));
         });
